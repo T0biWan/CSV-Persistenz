c050b2a8b9bb74dfd1d71cc32c0bf94b58200f7f
Robert Dziuba <robertodziuba@gmail.com>
2015-06-17 14:08:14 +0200

    exception behandeln

--- src/csvio/CSVContactsReader.java
+++ src/csvio/CSVContactsReader.java
@@ -2,19 +2,19 @@ package csvio;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import classes.ContactDetails;
 import classes.ObservableContactDetails;
 import exceptions.ContactFormatException;
 
 public class CSVContactsReader {
 	
 	/* Helferklasse, die uns ermöglicht, filename als String zu übergeben - ruft dann die untere readEntityList() aus */
-	public static ObservableContactDetails[] readEntityList(String dateiname, String splitter){
+	public static ObservableContactDetails[] readEntityList(String dateiname, String splitter) throws NoSuchFileException{
 		Path source = Paths.get(dateiname);
 		return readEntityList(source, splitter);
 	}
@@ -23,7 +23,7 @@ public class CSVContactsReader {
 	 * wie beim Writer nur andersrum lesen wir unsere Datei ein 
 	 * und kreieren uns aus den eingelesenen Strings wieder ein Objekt. 
 	 * */
-	public static ObservableContactDetails[] readEntityList(Path source, String splitter){
+	public static ObservableContactDetails[] readEntityList(Path source, String splitter) throws NoSuchFileException{
 		List<ObservableContactDetails> target = new ArrayList<>();
 		try {
 			List<String> lines = Files.readAllLines(source);
@@ -37,8 +37,9 @@ public class CSVContactsReader {
 					e.printStackTrace();
 				}
 			}
-		} catch (IOException ex) { ex.printStackTrace(System.err);
+		} catch (IOException ex) {
 			target.addAll(null); //null addition to target to indicate problem
+			throw new NoSuchFileException(source.toString());
 		}
 		
 		return target.toArray(new ObservableContactDetails[target.size()]);
