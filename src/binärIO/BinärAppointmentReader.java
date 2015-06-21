f458d2813d98b3fcadf1386fc5aa7963f4074a30
T0biWan <Tobi.Klatt@web.de>
2015-06-21 13:23:50 +0200

    Code ist kommentiert

--- src/binärIO/BinärAppointmentReader.java
+++ src/binärIO/BinärAppointmentReader.java
@@ -16,11 +16,16 @@ import exceptions.ZeitenKollisionException;
 
 public class BinärAppointmentReader {
 	
+	//Path wird generiert.
+	//Aus der Textdatei werden die Daten als fortlaufender einzeiliger String eingelesen.
+	//Mit der Methode split() wird der String aufgeteilt in Attribute.
+	//Diese werden indexweise in einem Array gespeichert.
+	//Da wir wissen wie viele Attribute ein Objekt hat, gehen wir es index für index durch und erstellen Objekte aus den Attributen.
+	//Die Objekte werden einer Collection hinzugefügt die, nach dem schließen des Streams, zurückgegeben wird.
 	public List<Appointment> readAppointment(String dateiname, String splitter) throws IOException, FormatException, WertebereichException, StringIsEmptyException, ZeitenKollisionException {
 		String path = "output/" + dateiname + ".bin";
 		List<Appointment> inputCollection = new ArrayList();
 		DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(path)));
-		
 		String inputString = input.readUTF();
 		String[] attribute = inputString.split(splitter);
 		int index = 0;
